b1c471f3343cbbea8e1d41ff77876a20ff33bdc2
Curtis Gagliardi <gagliardi.curtis@gmail.com>
2012-04-20 21:10:42 -0500

    Finish static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,14 @@
 class StaticPagesController < ApplicationController
 
   def home
+    @title = 'Home'
   end
 
   def help
+    @title = 'Help'
+  end
+
+  def about
+    @title = 'About Us'
   end
 end
