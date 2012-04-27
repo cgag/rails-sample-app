99badaee9d3e5d3e85d2072731cb9998631fcc67
Curtis Gagliardi <gagliardi.curtis@gmail.com>
2012-04-26 22:45:56 -0500

    authorization and authentication

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -66,4 +66,46 @@ describe "User Pages" do
     it { should have_selector('h1',     text: user.name) }
     it { should have_selector('title',  text: user.name) }
   end
+
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      sign_in user 
+      visit edit_user_path(user)
+    end
+
+    describe "page" do
+      it { should have_selector('h1', text: "Update your profile") }
+      it { should have_selector('title', text: "Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_content('error') }
+    end
+
+    describe "with valid information" do
+      let(:new_name)  { "jyoung" }
+      let(:new_email) { "fake@email.com" }
+
+      before do
+        fill_in 'Name',             with: new_name
+        fill_in 'Email',            with: new_email
+        fill_in 'Password',         with: user.password
+        fill_in 'Confirm Password', with: user.password
+        click_button 'Save changes'
+      end
+
+      it { should have_selector('title', text: new_name) }
+      it { should have_selector('div.alert.alert-success') }
+      # why? -- because reset token gets reset on update save
+      # so we have to sign in again to reset the cookie
+      it { should have_link('Sign out', href: signout_path) } 
+      specify { user.reload.name.should  == new_name }
+      specify { user.reload.email.should == new_email }
+    end
+
+  end
 end
