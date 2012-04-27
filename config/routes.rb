8aa177dcf5a83ca7c28b6d9dd4fa983974a8cf18
Curtis Gagliardi <gagliardi.curtis@gmail.com>
2012-04-26 19:02:20 -0500

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SampleApp::Application.routes.draw do
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   root to: 'static_pages#home'
 
@@ -9,6 +10,8 @@ SampleApp::Application.routes.draw do
   match '/contact', to: 'static_pages#contact'
 
   match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
